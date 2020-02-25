d0e02c2a3941fd136fd5ea6726f493dace8e7f43
hi2048 <291882199@qq.com>
2020-02-25 15:58:01 +0800

    modify demo.

--- site/examples/src/index.js
+++ site/examples/src/index.js
@@ -4,7 +4,12 @@ import { Stepper, DynamicNumber } from '../../src/components';
 
 ReactDOM.render(
   <div>
+    Stepper: 
     <Stepper stepping={100}></Stepper>
+    <div style={{ clear: 'both' }}></div>
+    <hr />
+
+    DynamicNumber: 
     <DynamicNumber speed={'faster'} stepping={10}></DynamicNumber>
     <DynamicNumber speed={'fast'}></DynamicNumber>
     <DynamicNumber speed={'normal'}></DynamicNumber>
